04c6281c303441686ab9e543cc01a4376aa75f3b
victor venegas <victor.venegas.call@gmail.com>
2024-10-21 21:34:32 -0300

    [add]: primera parte del trabajador

--- src/core/routes/AppRouter.tsx
+++ src/core/routes/AppRouter.tsx
@@ -1,10 +1,17 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { LoginView } from "../../auth/views/LoginView";
+import { HomeViewJefe } from "../../jefe/views/HomeViewJefe";
+import { Navbar } from "../../components/Navbar";
 
 export const AppRouter = () => {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginView />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<LoginView />} />
+        <Route path="/home-jefe" element={<HomeViewJefe />} />
+        <Route path="*" element={<Navigate to="/home-jefe" />} />
+      </Routes>
+    </>
   );
 };
