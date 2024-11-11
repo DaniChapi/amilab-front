fe4ebc2a3253128e8f8bd64eac29987e34556383
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2024-11-11 12:37:08 -0300

    [add]: avance en la vista del cliente

--- src/components/ui/Acordeon.tsx
+++ src/components/ui/Acordeon.tsx
@@ -1,5 +1,4 @@
 import {
-  Children,
   PropsWithChildren,
   useEffect,
   useRef,
--- src/jefe/views/Clientes.tsx
+++ src/jefe/views/Clientes.tsx
@@ -1,7 +1,30 @@
 export const Clientes = () => {
   return (
-    <div>
-      <h1>Clientes</h1>
+    <div className="p-4">
+      <div className="flex flex-col">
+        <select className="w-full mb-4 border-blue-200  p-2 rounded-md">
+          <option value="nada" selected disabled>Seleccione un cliente</option>
+          <option className="w-full" value="ClientesFrecuentes">Clientes Frecuentes</option>
+          <option  className="w-full" value="ClienteNoCompra">Clientes Sin compras este año</option>
+          <option  className="w-full" value="ClienteConCompra">Clientes con compra frecuente</option>
+        </select>
+        <select className="w-full mb-4 border-blue-200  p-2 rounded-md">
+          <option value="nada" selected disabled>Seleccione un vendedor</option>
+          <option className="w-full" value="vendedorMMG">Vendedor MMG</option>
+          <option  className="w-full" value="vendedorHGC">Vendedor HGC</option>
+          <option  className="w-full" value="vendedorGCP">Vendedor GCP</option>
+        </select>
+      </div>
+
+      <div className="bg-blue-300 p-4 rounded-md text-[#333333]">
+        <p className="text-center text-xl mb-2">Nombre empresa</p>
+        <ul className="flex flex-col justify-between">
+          <li className="flex justify-between"><p>Debe:</p> $150.000</li>
+          <li className="flex justify-between"><p>Rut empresa:</p><span>99.999.999-9</span></li>
+          <li className="flex justify-between"><p>Ultima compra:</p> 12/12/2024</li>
+          <li className="flex justify-between"><p>Total compras mensuales:</p> $1000.000</li>
+        </ul>
+      </div>
     </div>
   );
 };
