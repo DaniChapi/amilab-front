6f73bfffb01ae8ae2843005f81c2c40b6e80b779
DaniChapi <dchiappe@utem.cl>
2024-11-14 13:20:53 -0300

    Analisis de ventas

--- src/jefe/views/Ventas.tsx
+++ src/jefe/views/Ventas.tsx
@@ -1,7 +1,33 @@
-export const Ventas = () => {
+// Ventas.tsx
+import React, { useState } from 'react';
+import VentasTotales from './VentasTotales';
+import VentasPorVendedor from './VentasPorVendedor';
+import './App.css';
+
+const Ventas: React.FC = () => {
+  const [showVentasPorVendedor, setShowVentasPorVendedor] = useState(false);
+
+  // Función para alternar entre las vistas
+  const toggleView = () => {
+    setShowVentasPorVendedor(!showVentasPorVendedor);
+  };
+
   return (
-    <div>
-      <h1>Ventas</h1>
+    <div className="app">
+      <header className="header">
+        <h1 className="ventas-title">Ventas</h1>
+      </header>
+
+      <section className="content">
+        {/* Renderizado condicional de las vistas */}
+        {showVentasPorVendedor ? (
+          <VentasPorVendedor onToggleView={toggleView} />
+        ) : (
+          <VentasTotales onToggleView={toggleView} />
+        )}
+      </section>
     </div>
   );
 };
+
+export default Ventas;
--- src/jefe/views/index.ts
+++ src/jefe/views/index.ts
@@ -1,3 +1,5 @@
 export * from "./Clientes";
 export * from "./HomeViewJefe";
 export * from "./Ventas";
+export { default as Ventas } from './Ventas';
+
